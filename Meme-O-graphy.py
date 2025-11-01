e5a2af648768ea70b3d5a36b362c0d7b3a964610
texe2036 <eliott.texier@hotmail.com>
2025-11-01 10:45:28 -0400

    Added destroy

--- Meme-O-graphy.py
+++ Meme-O-graphy.py
@@ -57,18 +57,21 @@ class MemeOGraphy :
         self.image.configure(image = img_tk)
 
     def _initCryption(self) :
-        self.initCryption.destroy
-        self.outputDecryption = ctk.CTkTextbox(self.dataFrame, width = 400, corner_radius = 0)
-        self.outputDecryption.grid(row = 0, column = 0, sticky = "nsew")
-        self.outputDecryption.insert("0.0", "Bye")
-        self.outputDecryption.grid(row = 0,column = 0,padx = 5,pady = 5)
+        self.initCryption.destroy()
+        self.inputCryption.configure(state="disabled")
+        entry = self.inputCryption.get()
+        #pierreEtienneBrindle(entry)
+        self.outputDecryption = ctk.CTkTextbox(self.dataFrame, width = 100, corner_radius = 0)
+        self.outputDecryption.grid(row = 0, column = 3, sticky = "nsew")
+        self.outputDecryption.insert("0.0", "Cryption started")
+        self.outputDecryption.grid(row = 0,column = 3,padx = 5,pady = 5)
         
         
     def _initDecryption(self) :
         self.outputDecryption = ctk.CTkTextbox(self.dataFrame, width = 400, corner_radius = 0)
-        self.outputDecryption.grid(row = 0, column = 0, sticky = "nsew")
+        self.outputDecryption.grid(row = 1, column = 3, sticky = "nsew")
         self.outputDecryption.insert("0.0", "We're no strangers to love You know the rules and so do IA full commitment's what I'm thinking of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you (Ooh, give you up) (Ooh, give you up) Never gonna give, never gonna give (Give you up) Never gonna give, never gonna give (Give you up) We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you")
-        self.outputDecryption.grid(row = 0,column = 0,padx = 5,pady = 5)
+        self.outputDecryption.grid(row = 1,column = 3,padx = 5,pady = 5)
 
 
 if __name__ == '__main__':
